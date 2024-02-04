0893da07cf3c239c7b4f6baab2e1714e1057b3fc
Suiiinaldo <toprakhar03@gmail.com>
2024-02-05 00:39:30 +0530

    Updated Main index File

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { PORT } from "./config/server-config.js";
 import { connection as Connect } from "./config/database-config.js";
-import TweetRepository from "./repository/tweet-repository.js";
 
 const app = express();
 
@@ -9,18 +8,5 @@ app.listen(PORT, async() => {
     console.log(`Server Successfully started at PORT : ${PORT}`);
     await Connect();
     console.log("Mongo Connected");
-
-
-    // Tweet.create({
-    //     content: "This is my first Tweet",
-    //     likes: 25,
-    //     noOfRetweets: 4,
-    // })
-    // const tweetRepo = new TweetRepository();
-    // const tweets = await tweetRepo.getAllTweets();
-    // console.log(tweets);
-    // const tweet = await tweetRepo.getTweet('65bf6e1dba4fadd98e12bf0e');
-    // const response = await tweetRepo.getAll({});
-    // console.log(response);
     
 })
